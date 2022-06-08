ca03f05637f3443797a124661822ea23e9b33205
curry0622 <58351529+curry0622@users.noreply.github.com>
2022-06-08 12:03:44 +0800

    feat: connect ui to functions

--- controller.py
+++ controller.py
@@ -4,14 +4,22 @@ from UI import Ui_MainWindow
 
 class MainWindow_controller(QtWidgets.QMainWindow):
     def __init__(self):
-        super().__init__() # in python3, super(Class, self).xxx = super().xxx
+        super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setup_control()
 
     def setup_control(self):
-        # self.ui.pushButton.clicked.connect(self.test_func)
+        self.ui.select_choose.currentIndexChanged.connect(self.handle_select_change)
+        self.ui.btn_sql.clicked.connect(self.handle_sql_query)
         return
 
-    def test_func(self):
-        print('test')
+    def handle_select_change(self, i):
+        self.ui.label_result.setText('Result:\n' + self.ui.select_choose.currentText())
+        print(i, self.ui.select_choose.currentText())
+        return
+
+    def handle_sql_query(self):
+        self.ui.label_result.setText('Result:\n' + self.ui.input_sql.text())
+        print(self.ui.input_sql.text())
+        return
