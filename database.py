f669a4062951b1cd723f8b069202dbd8c99afd3f
curry0622 <58351529+curry0622@users.noreply.github.com>
2022-06-08 16:54:52 +0800

    feat: basic db connect sample

--- database.py
+++ database.py
@@ -5,7 +5,12 @@ class Database:
         self.db = mysql.connector.connect(
             host='localhost',
             user='root',
-            passwd='',
-            # database=''
+            passwd='A142536',
+            database='sql_test'
         )
         self.cursor = self.db.cursor()
+
+if __name__ == '__main__':
+    db = Database()
+    db.cursor.execute('SELECT * FROM `student`')
+    print(db.cursor.fetchall())
